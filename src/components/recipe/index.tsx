39bd700e19d7032600192c902eba19366ff94585
Felipe Jonathan <felipejonathan.dev@gmail.com>
2024-03-05 16:27:47 -0300

    feat: creating the recipe component.

---
+++ src/components/recipe/index.tsx
@@ -0,0 +1,46 @@
+import { ImageBackground, Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { style } from "./style";
+import { LinearGradient } from "expo-linear-gradient";
+
+type RecipeProps = TouchableOpacityProps & {
+    recipe: {
+        name: string,
+        image: string,
+        time: number
+    }
+}
+
+export function Recipe({ recipe, ...rest }: RecipeProps) {
+    return (
+        <TouchableOpacity
+            style={ style.container }
+            activeOpacity={ 0.8 }
+            { ...rest }
+        >
+            <ImageBackground
+                source={{ uri: recipe.image }}
+                style={ style.image }
+            >
+                <LinearGradient
+                    colors={[ "rgba(0,0,0,0.5)", "#000" ]}
+                    style={ style.linear }
+                >
+                    <Text
+                        style={ style.title }
+                        numberOfLines={ 1 }
+                        lineBreakMode="tail"
+                    >
+                        { recipe.name }
+                    </Text>
+                    <Text
+                        style={ style.time }
+                        numberOfLines={ 1 }
+                        lineBreakMode="tail"
+                    >
+                        { recipe.time }
+                    </Text>
+                </LinearGradient>
+            </ImageBackground>
+        </TouchableOpacity>
+    );
+}
---
+++ src/components/recipe/style.ts
@@ -0,0 +1,31 @@
+import { theme } from "@/theme";
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    container: {
+        // flex: 1,
+        
+        height: 192,
+        
+        borderRadius: theme.borderRadius.lg,
+        overflow: "hidden"
+    },
+    image: {
+        flex: 1
+    },
+    linear: {
+        flex: 1,
+        padding: 12,
+        justifyContent: "flex-end"
+    },
+    title: {
+        color: theme.colors.white,
+        fontSize: theme.fonts.size.body.sm,
+        fontFamily: theme.fonts.family.bold
+    },
+    time: {
+        color: theme.colors.yellow_500,
+        fontSize: theme.fonts.size.body.xs,
+        fontFamily: theme.fonts.family.regular
+    }
+});
