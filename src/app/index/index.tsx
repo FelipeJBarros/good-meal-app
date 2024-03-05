712e8159e211b8741294e4d4a5c5224f9c37bfe5
Felipe Jonathan <felipejonathan.dev@gmail.com>
2024-03-05 10:23:54 -0300

    feat: developing the home screen header.

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,12 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableHighlight } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
 
 import { style } from "./style";
+import { theme } from "@/theme";
 
 export default function Home() {
     return (
         <View style={ style.container }>
-            <Text>
-                Good Meal AI
+            <View style={ style.row }>
+                <Text style={ style.title }>
+                    Find {"\n"}
+                    <Text style={ style.subtitle }>
+                        ingredients
+                    </Text>
+                </Text>
+                <TouchableHighlight style={ style.photoButton }>
+                    <MaterialIcons
+                        name="add-a-photo"
+                        size={20}
+                        color={ theme.colors.white }
+                    />
+                </TouchableHighlight>
+            </View>
+            <Text style={ style.message }>
+                Discover recipes based on the products {"\n"}
+                you chose or through images.
             </Text>
         </View>
     );
--- src/app/index/style.ts
+++ src/app/index/style.ts
@@ -1,9 +1,41 @@
+import { theme } from "@/theme";
 import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 24
+    },
+    title: {
+        fontSize: theme.fonts.size.heading.lg,
+        fontFamily: theme.fonts.family.bold,
+        lineHeight: 28
+    },
+    subtitle: {
+        fontFamily: theme.fonts.family.regular
+    },
+    message: {
+        fontSize: theme.fonts.size.body.sm,
+        fontFamily: theme.fonts.family.regular,
+        marginTop: 12,
+        marginBottom: 36,
+        color: theme.colors.gray_400
+    },
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
         alignItems: "center",
+        marginTop: 42
+    },
+    photoButton: {
+        backgroundColor: theme.colors.green_600,
+        
+        padding: 12,
+        width: 48,
+        height: 48,
+        borderRadius: theme.borderRadius.md,
+
         justifyContent: "center",
+        alignItems: "center"
     }
 });
