9be903ca0a528517191ebed7803bd808640f2019
Felipe Jonathan <felipejonathan.dev@gmail.com>
2024-03-06 10:36:52 -0300

    feat: adding recipes services.

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,9 @@
 import * as ingredients from "./ingredientsService";
+import * as recipes from "./recipesService";
 
 export const services = {
     ingredients,
+    recipes,
     storage: {
         imagePath: "https://xyidsnkxgexwjteantfd.supabase.co/storage/v1/object/public/Ingredients"
     }
